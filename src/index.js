0ba7df1008f8459b16275e8e28dde77e83236bef
broteinheit <manueleiwen58@gmail.com>
2019-05-23 14:23:12 +0200

    fix showing other clients (as client);

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var users = [];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    users.push(socket.id);
     console.log('a user connected');
     io.to(socket.id).emit('id', socket.id);
 
+    io.sockets.emit('users changed', users);
+
     socket.on('disconnect', function(){
       console.log('user disconnected');
+      users.splice(users.indexOf(socket.id), 1);
+      socket.broadcast.emit('users changed', users);
     });
 
     socket.on('orientation', function(msg) {
       console.log(msg);
-      socket.broadcast.emit('orientation', msg);
+      socket.broadcast.emit('others', msg);
     });
 });
 
