1da5b23a2b1318acaa2c5335949e025005632fc9
broteinheit <manueleiwen58@gmail.com>
2019-05-23 17:31:21 +0200

    switch to namespaces for clients/servers;

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var users = [];
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -15,24 +15,44 @@ app.get('/server', function(req, res) {
     res.sendFile(__dirname + "/server.html");
 });
 
-io.on('connection', function(socket){
-    users.push(socket.id);
-    console.log('a user connected');
-    io.to(socket.id).emit('id', socket.id);
+io.of('/server').on('connection', function(socket) {
+    console.log('a server connected');
 
-    io.sockets.emit('users changed', users);
 
     socket.on('disconnect', function(){
-      console.log('user disconnected');
-      users.splice(users.indexOf(socket.id), 1);
+      console.log('server disconnected');
+    });
+});
+
+var clients = io.of('/client');
+//CLIENTS
+clients.on("connection", function(socket) {
+    console.log('a client connected');
+
+    
+    var id = socket.id;
+    id = id.substring(id.indexOf('#')+1, id.length)
+
+    socket.emit('id', id);
+    users.push(id);
+
+    socket.broadcast.emit('users changed', users);
+    socket.emit('users changed', users);
+
+    socket.on('disconnect', function(){
+      console.log('client disconnected');
+      users.splice(users.indexOf(id), 1);
       socket.broadcast.emit('users changed', users);
     });
 
     socket.on('orientation', function(msg) {
-      console.log(msg);
+      //console.log(users);
       socket.broadcast.emit('others', msg);
     });
 });
+
+
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
