5637ea72a5b44439425a2673234ddddb0fbfd9c2
Ida Hoenigmann <hoenigmann.ida@student.htlwrn.ac.at>
2019-05-30 11:37:00 +0200

    refactor;

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var users = [];
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const users = [];
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/client', function(req, res) {
+app.get('/client', function (req, res) {
     res.sendFile(__dirname + "/client.html");
 });
 
-app.get('/server', function(req, res) {
+app.get('/server', function (req, res) {
     res.sendFile(__dirname + "/server.html");
 });
 
-app.get('/js/three.js', function(req, res) {
+app.get('/js/three.js', function (req, res) {
     res.sendFile(__dirname + "/js/three.js");
 });
 
-app.get('/js/OrbitControls.js', function(req, res) {
+app.get('/js/OrbitControls.js', function (req, res) {
     res.sendFile(__dirname + "/js/OrbitControls.js");
 });
 
 
-var serverSockets = io.of('/server')
-serverSockets.on('connection', function(socket) {
+const serverSockets = io.of('/server');
+serverSockets.on('connection', function (socket) {
     console.log('a server connected');
     socket.emit('users changed', users);
 
-    socket.on('disconnect', function(){
-      console.log('server disconnected');
+    socket.on('disconnect', function () {
+        console.log('server disconnected');
     });
 });
 
-var clients = io.of('/client');
+const clients = io.of('/client');
 //CLIENTS
-clients.on("connection", function(socket) {
+clients.on("connection", function (socket) {
     console.log('a client connected');
 
-    
+
     var id = socket.id;
-    id = id.substring(id.indexOf('#')+1, id.length)
+    id = id.substring(id.indexOf('#') + 1, id.length);
 
     socket.emit('id', id);
     users.push(id);
@@ -49,25 +49,22 @@ clients.on("connection", function(socket) {
     socket.broadcast.emit('users changed', users);
     socket.emit('users changed', users);
 
-    serverSockets.emit('users changed', users);    
+    serverSockets.emit('users changed', users);
 
-    socket.on('disconnect', function(){
-      console.log('client disconnected');
-      users.splice(users.indexOf(id), 1);
-      socket.broadcast.emit('users changed', users);
-      serverSockets.emit('users changed', users);
+    socket.on('disconnect', function () {
+        console.log('client disconnected');
+        users.splice(users.indexOf(id), 1);
+        socket.broadcast.emit('users changed', users);
+        serverSockets.emit('users changed', users);
     });
 
-    socket.on('orientation', function(msg) {
-      //console.log(users);
-      socket.broadcast.emit('others', msg);
-      serverSockets.emit('orientation', msg);
+    socket.on('orientation', function (msg) {
+        //console.log(users);
+        socket.broadcast.emit('others', msg);
+        serverSockets.emit('orientation', msg);
     });
 });
 
-
-
-
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
